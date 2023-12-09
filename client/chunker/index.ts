48e440e680cb801eeb8383aef63bb8057dcb3d6f
Nigel Lee <ticklepoke@users.noreply.github.com>
2023-12-09 16:16:09 +0800

    feat: file chunker

---
+++ client/chunker/index.ts
@@ -0,0 +1,29 @@
+import { readFileSync } from 'fs';
+import path from 'path';
+
+const CHUNK_SIZE = 1024 * 1024; // bytes
+
+function chunkBuffer(buffer: Buffer) {
+	const bufferSize = buffer.byteLength;
+	let offset = 0;
+
+	while (offset < bufferSize) {
+		const chunk = buffer.subarray(offset, offset + CHUNK_SIZE);
+		console.log(chunk, offset);
+		offset += chunk.byteLength;
+	}
+	console.log('done reading file');
+}
+
+function readBuffer(fileName: string) {
+	const buffer = readFileSync(path.join(process.cwd(), '../../../', 'Downloads/VID_20231129_172444_00_002.mp4'));
+
+	return buffer;
+}
+
+function main() {
+	const buffer = readBuffer('foo');
+	chunkBuffer(buffer);
+}
+
+main();
